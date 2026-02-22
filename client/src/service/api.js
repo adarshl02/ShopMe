851ba75f2f80453a4634e8bd0b826207129d2344
adarshl02 <adarsh.landge10604@gmail.com>
2026-02-22 20:54:13 +0530

    Update backend URL in .env and api.js; add .env to .gitignore

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const URL='/api';         //our react app is on 3000 but but are routing to 8000/signup
+const URL= process.env.REACT_APP_BACKEND_URL ;         //our react app is on 3000 but but are routing to 8000/signup
 
 
 
